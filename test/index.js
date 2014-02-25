a89d169187295121443f0b5dedd93a37f34ccc87
Joaquim Serafim <joaquim.serafim@gmail.com>
2014-02-25 14:00:14 +0000

    Removed tape for testing, using assert

--- test/index.js
+++ test/index.js
@@ -1,25 +1,34 @@
-var test = require('tape');
-
+var assert = require('assert');
 var load = require('../');
 
+[
+  function (cb) {
+    console.log('# load heroku env');
 
-test('load Heroku env', function (t) {
-  t.plan(2);
+    load('heroku', './test/config');
 
-  load('heroku', './test/config');
+    assert.deepEqual(process.env.MONGODB_URL, 'mongodb://xpto:password@ec2-22-197-555-120.compute-100.amazonaws.com/lilidb?numberOfRetries=10&retryMiliSeconds=10000', 'Your db conn str is ' + process.env.MONGODB_URL);
 
-  t.deepEqual(process.env.MONGODB_URL, 'mongodb://xpto:password@ec2-22-197-555-120.compute-100.amazonaws.com/lilidb?numberOfRetries=10&retryMiliSeconds=10000', 'Your db conn str is ' + process.env.MONGODB_URL);
+    assert.deepEqual(Number(process.env.APP_PORT), 4000, 'Your web app run @ ' +
+      process.env.APP_PORT);
 
-  t.deepEqual(Number(process.env.APP_PORT), 4000, 'Your web app run @ ' + process.env.APP_PORT);
-});
+    cb(null, 'keroku');
+  },
+  function (cb) {
+    console.log('# load development env');
 
+    load('./test/config');
 
-test('load development env', function (t) {
-  t.plan(2);
+    assert.deepEqual(process.env.MONGODB_URL, 'mongodb://xpto:111@localhost/lilidb?numberOfRetries=10&retryMiliSeconds=10000', 'Your db conn str is ' + process.env.MONGODB_URL);
 
-  load('./test/config');
+    assert.deepEqual(Number(process.env.APP_PORT), 5000, 'Your web app run @ ' +
+      process.env.APP_PORT);
 
-  t.deepEqual(process.env.MONGODB_URL, 'mongodb://xpto:111@localhost/lilidb?numberOfRetries=10&retryMiliSeconds=10000', 'Your db conn str is ' + process.env.MONGODB_URL);
-
-  t.deepEqual(Number(process.env.APP_PORT), 5000, 'Your web app run @ ' + process.env.APP_PORT);
+    cb(null, 'development');
+  }
+].forEach(function (test) {
+  test(function (err, t) {
+    console.log('# finish ' + t);
+    console.log();
+  });
 });
